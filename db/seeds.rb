24cd98bd5ad1cb61ea017b5f40202b4c9ce225d1
John Tuttle <thejwt@gmail.com>
2017-12-04 07:49:19 +0000

    user class

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,14 @@
-10.times do 
+10.times do
   Item.create(
-    title: Faker::Commerce.product_name, 
-    inventory: Faker::Number.number(2), 
+    title: Faker::Commerce.product_name,
+    inventory: Faker::Number.number(2),
     price: Faker::Number.number(4)
   )
   Category.create(title: Faker::Commerce.department)
 end
+
+User.create!(email: "guy@gmail.com", password: "111111111")
+#binding.pry
 
 counter = 1
 Item.all.each do |item|
