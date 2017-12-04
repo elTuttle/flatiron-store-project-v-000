a7b09e02d6c1beaebcb6ad7379996c13b6bc3d6f
John Tuttle <thejwt@gmail.com>
2017-12-04 09:48:38 +0000

    line items controller

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -5,7 +5,20 @@ class LineItemsController < ApplicationController
       @cart = current_user.carts.create
       current_user.current_cart = @cart
     end
-    current_user.current_cart.line_items << LineItem.find_or_create_by(item_id: params[:item_id])
+
+    current_user.current_cart.line_items << LineItem.find_or_create_by(item_id: params[:item_id], cart_id: current_user.current_cart.id)
+    #binding.pry
+    li = current_user.current_cart.line_items.find_by(item_id: params[:item_id])
+    if li.quantity.nil?
+      li.quantity = 1
+    else
+      li.quantity += 1
+    end
+    li.save
+    current_user.current_cart.save
+    current_user.save
+
+    redirect_to cart_path(current_user.current_cart)
   end
 
 end
