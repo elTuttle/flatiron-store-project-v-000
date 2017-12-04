0cd26231ca01607a31d1a76966b8a8a4b154e379
John Tuttle <thejwt@gmail.com>
2017-12-04 05:38:57 +0000

    working on cart class

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,19 @@
 class Cart < ActiveRecord::Base
+  belongs_to :user
+  has_many :line_items
+  has_many :items, through: :line_items
+
+  def total
+    price = 0
+    self.items.each do |item|
+      price += item.price
+    end
+    price
+  end
+
+  def add_item(new_item)
+
+
+  end
 
 end
