65b16a1eb1942a1869d153a366e5c861cb620f6e
John Tuttle <thejwt@gmail.com>
2017-12-04 07:03:10 +0000

    finished Cart class

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -11,8 +11,15 @@ class Cart < ActiveRecord::Base
     price
   end
 
-  def add_item(new_item)
-
+  def add_item(item_id)
+    if self.line_items.find_by(item_id: item_id)
+      temp_item = self.line_items.find_by(item_id: item_id)
+      #binding.pry
+      temp_item.update(quantity: temp_item.quantity + 1)
+      temp_item
+    else
+      LineItem.new(item_id: item_id, cart_id: self.id, quantity: 1)
+    end
 
   end
 
