e9a18a075752b32d602bb16f2eba31569b116407
John Tuttle <thejwt@gmail.com>
2017-12-04 08:50:15 +0000

    working on cart controller

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,3 +1,18 @@
 class CartsController < ApplicationController
 
+  def show
+  end
+
+  def checkout
+    @cart = Cart.find(params[:id])
+    @cart.line_items.each do |line_items|
+      line_items.item.inventory -= line_items.quantity
+      line_items.item.save
+    end
+    current_user.current_cart = nil
+    current_user.save
+    #binding.pry
+    redirect_to cart_path(@cart)
+  end
+
 end
